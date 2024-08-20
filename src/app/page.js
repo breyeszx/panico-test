cbe255880cea9224069f324b3f33927dd9eefe22
breyeszx <breyes1864@gmail.com>
2024-08-20 18:27:27 -0400

    'lol'

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero from "../components/hero";
+import LoginPage from "../components/login";
 
 export default function Home() {
-  return <Hero />;
+  return <LoginPage />;
 }
