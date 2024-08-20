b5187fd834c991977931668d5f6ecfef0bd22431
breyeszx <breyes1864@gmail.com>
2024-08-20 19:04:13 -0400

    ssr changes

--- src/app/panico/page.js
+++ src/app/panico/page.js
@@ -1,5 +1,8 @@
 import Hero from "@/src/components/hero";
 import React from "react";
+import dynamic from "next/dynamic";
+
+const Hero = dynamic(() => import("@/src/components/hero"), { ssr: false });
 
 export default function Home() {
   return <Hero />;
