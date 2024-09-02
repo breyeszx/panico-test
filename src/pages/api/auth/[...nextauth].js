939232595eb7339e31e61c0c79930822e2119fad
breyeszx <breyes1864@gmail.com>
2024-09-02 14:15:07 -0400

    mongofix

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { verifyPassword } from "../auth/auth";
-import { dbConnect } from "../../../lib/mongodb";
+import { dbConnect } from "../../../../lib/mongodb";
 
 export default NextAuth({
   session: {
